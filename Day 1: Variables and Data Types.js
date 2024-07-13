0c10142c45a6720caea49b9dfbef12a6b551ccb3
Ashutosh Sarangi <ashutoshsarangi95@gmail.com>
2024-07-13 13:37:34 +0300

    Create Day1_VariablesandDataTypes.js

---
+++ Day 1: Variables and Data Types.js
@@ -0,0 +1,92 @@
+
+
+   //Day 1: Variables and Data Types 
+
+//   Tasks/Activities: 
+
+//   Activity 1: Variable Declaration 
+
+    // Task 1: Declare a variable using var, assign it a number, and log the value to the console. 
+    
+    var varVariable = 123;
+    console.log('varVariable ---> ', varVariable);
+
+    // Task 2: Declare a variable using let, assign it a string, and log the value to the console. 
+    var letVariable = 'hello world';
+    console.log('varVariable ---> ', letVariable);
+    
+
+//   Activity 2: Constant Declaration 
+
+    // Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console
+    
+    var constVariable = false;
+    console.log('varVariable ---> ', constVariable);
+    
+
+//   Activity 3: Data Types 
+
+    
+
+    // Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator. 
+    const numberType = 123;
+    const stringType = '123';
+    const booleanType = false;
+    const objectType = {
+        name: 'Ashutosh'
+    }
+    const arrayType = [1, 2, 3];
+    
+    console.log('type of number Variable --> ', typeof numberType)
+    console.log('type of string Variable --> ', typeof stringType)
+    console.log('type of boolean Variable --> ', typeof booleanType)
+    console.log('type of object Variable --> ', typeof objectType)
+    console.log('type of array Variable --> ', typeof arrayType)
+
+
+//   Activity 4: Reassigning Variables 
+
+    
+
+    // Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console. 
+    let initialLetValue = 123;
+    initialLetValue = '123';
+    
+    console.log('initialLetValue --> ', initialLetValue);
+    
+
+//   Activity 5: Understanding const 
+
+    
+
+    // Task 6: Try reassigning a variable declared with const and observe the error. 
+
+    const constReassign = 123;
+    
+    constReassign = 34;
+
+//   Feature Request: 
+
+//     Variable Types Console Log: Write a script that declares variables of different data types and logs both the value and type of each variable to the console. 
+
+//     Reassignment Demo: Create a script that demonstrates the difference in behavior between let and const when it comes to reassignment. 
+
+    
+
+//   Achievement: 
+
+//   By the end of these activities, you will: 
+
+    
+
+//     Know how to declare variables using var, let, and const. 
+
+//     Understand the different data types in JavaScript. 
+
+//     Be able to use the typeof operator to identify the data type of a variable. 
+
+//     Understand the concept of variable reassignment and the immutability of const variables. 
+
+   
+
+  
