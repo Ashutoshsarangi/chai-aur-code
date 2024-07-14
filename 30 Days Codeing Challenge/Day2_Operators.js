40bd5d1f134bf414d86a3738a2351a60a1124b5f
Ashu <ashutoshsarangi95@gmail.com>
2024-07-14 21:07:04 +0300

    Initial Commit

---
+++ 30 Days Codeing Challenge/Day2_Operators.js
@@ -0,0 +1,11 @@
+// Challenge 1
+/* Arithmetic Operations Script: Write a script that performs basic arithmetic operations
+ (addition, subtraction, multiplication, division, remainder) on two numbers and logs the results. */
+
+// Challenge 2
+/* Comparison and Logical Operators Script: Create a script that compares two numbers using
+ different comparison operators and combines conditions using logical operators, logging the results.*/
+
+// Challenge 3
+/* Ternary Operator Script: Write a script that uses the ternary operator to determine
+ if a number is positive or negative and logs the result.*/
